acb7270d3e75461dfffa92c5c5a17aad233b0116
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-03-01 08:36:58 -0500

    initial commit

---
+++ Cow.cs
@@ -0,0 +1,17 @@
+﻿namespace GitHubPackageSample;
+
+public static class Cow
+{
+    public static void Say() => Console.WriteLine(
+    """
+     ________________________________________
+    / You are using my GitHub Package from   \
+    \ my repository. Why though?             /
+     ----------------------------------------
+            \   ^__^
+             \  (oo)\_______
+                (__)\       )\/\
+                    ||----w |
+                    ||     ||
+    """);
+}
